78a61b007afdb95cedcf8e0839454678f9bf96cd
Gabs-TI <108961886+Gabs-TI@users.noreply.github.com>
2025-06-23 18:30:53 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    // Define a URL base para facilitar os comandos cy.visit()
+    // Assim, você pode usar caminhos relativos no seu teste, ex:
+    // cy.visit('/practice-test-login/')
     baseUrl: "https://practicetestautomation.com",
+
+    // Função para configurar eventos do Node, como plugins, relatórios, etc.
+    // Por enquanto está vazia, mas pode ser usada para adicionar funcionalidades extras
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implementar event listeners aqui, se necessário
     },
   },
 });
-
-
